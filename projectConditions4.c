925b437acd4da65d05aecbf88d0675b50e83d298
othman <109024890+hotmanX813@users.noreply.github.com>
2023-09-14 14:53:08 +0100

    Add files via upload

--- projectConditions4.c
+++ projectConditions4.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
-    char equation[10];
-    printf("Entrer l'equation avec un inconu x (e.g. 2x^{2} + 5 = 0):");
-    scanf("%s", &equation);
-    for(int i = 0; i < 15; ++i) {
-        char solution = equation[i];
-        printf("%s",solution);
+    float a, b, c, d, e;
+    printf("Entrer les valeurs a, b et c \n ax^{2}+by+c  : a =");
+    scanf("%f", &a);
+    printf("\n ax^{2}+by+c  : b =");
+    scanf("%f", &b);
+    printf("\n ax + by + c  : c =");
+    scanf("%f", &c);
+    d = 4*a*c;
+    e = b*b;
+    float delta = e-d;
+    int delta1 = delta;
+    printf("Le Delta = %.2d\n",delta1);
+    if (delta1 < 0) {
+        printf ("Delta est negatif\n Pas de solution");
+    } else if (delta1 == 0) {
+        float s1;
+        s1 = -b / 2*a;
+        printf("Delta = 0 \n La solution est :%.2f", s1);
+    } else if (delta1 > 0) {
+        float s1,s2,f,g;
+        f = sqrtf(delta1);
+        g = 2*a;
+        s1 = (0 - b - f) / g;
+        s2 = (0 - b + f) / g;
+        printf("Delta > 0 \n Les solution sont :%.2f et %.2f", s1,s2);
     }
 }
